653d0e29a32fb022a232ba50fa0d2716731670df
Mim-Hakkani <mimhakkani4g@gmail.com>
2023-05-16 18:17:50 +0600

    fetch data query and show all data using redux rtk query is done

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,36 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+    const {data:videos,isLoading,isError} =useGetVideosQuery();
+
+    let content = null ; 
+
+    if(isLoading) {
+        content = <>
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        
+        
         </>
-    );
+    }
+
+    if(isError){
+        content = <Error message="There is an Error " />
+    }
+
+      if(!isLoading &&  !isError && videos.length ===0){
+        content = <Error message="No Videos are here  " />
+    }
+
+    if(!isLoading && !isError && videos.length>0){
+        content = videos.map((video,index)=><Video key = {index} video={video}/>)
+    }
+
+
+    return content ;
 }
